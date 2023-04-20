599f96ef40421c6e094ad9d055c89100cbaae33d
denjohn@qq.com <371363622@qq.com>
2023-04-20 18:15:40 +0800

    add text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Text } from "@nextui-org/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -28,18 +28,34 @@ export default function Home() {
       </div>
 
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"> */}
-      <div>
+      <div className="relative flex ">
+        <>
+          <Text
+            h1
+            size={60}
+            css={{
+              textGradient: "45deg, $blue600 -20%, $yellow600 50%",
+            }}
+            weight="bold"
+          >
+            Illuminate
+          </Text>
+          <Text h1 size={60} weight="bold">
+            Your
+          </Text>
+          <Text h1 size={60} weight="bold">
+            Ideas
+          </Text>
+        </>
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="https://luminlabsweb3.github.io/lumin-website/images/luminNight.png"
+          src="https://luminlabsweb3.github.io/lumin-website/images/LuminLogo.png"
           alt="LuminLabs Logo"
           width={300}
           height={300}
           priority
         />
       </div>
-
-      
     </main>
-  )
+  );
 }
